a73c1461018a192d31b79cf208869629ffc351bf
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-06 12:11:44 +0600

    multiplication

---
+++ multiplication-function.js
@@ -0,0 +1,19 @@
+//creating a multiplication table of for loop 13 problem 1;
+// let number = 13;
+// for(let i = 1; i <= 10; i++) {
+//     // multiply i with number
+//     let result = i * number;
+//     // display the result
+//     console.log(`${number} * ${i} = ${result}`);
+// }
+
+// creating a multiplication table of function 13 problem 1;
+function multiplication(number) {
+    for(let i = 1; i <= 10; i++) {
+        // multiply i with number
+        let result = i * number;
+        // display the result
+        console.log(`${number} * ${i} = ${result}`);
+    }
+}
+multiplication(13);
